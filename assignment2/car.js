1228195dd51f5cf58c74f87b86d0fd9e9645dca6
zherui-lin <zherui.lin@foxmail.com>
2022-12-25 16:47:57 -0800

    finish assignment2 part1

---
+++ assignment2/car.js
@@ -0,0 +1,20 @@
+function calculateTotal() {
+    var res = 0;
+    var form = document.getElementById("price-form");
+    res += parseInt(form.configuration.value);
+    res += parseInt(form.combo.value);
+    for (checkbox of form.getElementsByTagName("input")) {
+        if (checkbox.type === "checkbox" && checkbox.checked) {
+            res += parseInt(checkbox.value);
+        }
+    }
+    // optinal parameters should be wrapped in a single object as parameter
+    form.result.value = "$" + res.toLocaleString(undefined, {minimumFractionDigits: 2});
+}
+
+function changeColor() {
+    document.getElementById("car-picture").src = document.getElementById("color").value + ".jpg";
+}
+
+document.getElementById("calculate").addEventListener("click", calculateTotal);
+document.getElementById("color").addEventListener("change", changeColor);
